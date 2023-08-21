ea2b172c76658f7538cf5c963878276aead27059
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-08-21 11:52:58 -0700

    fh

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverser a string
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int 1, i;
+	char temp;
+
+	/*find string length without null char*/
+	for (1 = 0; s[1] != '\0'; ++1)
+		;
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < 1 / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[1 - 1 - i]; /*=1 becomes the array starts from o*/
+		s[1 - 1 - i] = temp;
+	}
+}
