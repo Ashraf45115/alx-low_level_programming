01e53bb87096a435b83d1f47d262312fbb358b3a
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-08-24 14:58:20 -0700

    ksdi

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,14 +5,14 @@
  *
  * @a: an array of intergers
  * @n: the number of elements to swap
- * Return: nothing
+ * Return: nothing.
 */
 
 void reverse_array(int *a, int n)
 {
 	int i, j, t;
 
-	for (i = 0, j = (n - 1); i < j; i++, j++)
+	for (i = 0, j = (n - 1); i < j; i++, j--)
 	{
 		t = a[i];
 		a[i] = a[j];
