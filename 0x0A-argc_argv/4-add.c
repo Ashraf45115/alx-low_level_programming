c7b0a877bae9e368622af160b39bce383344b837
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-09-01 13:46:58 -0700

    dk

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the result, followed by a new line
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
