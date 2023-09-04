ad8596aa29a44276abc587f55d4f6f2dfb9341f9
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-09-04 13:06:56 -0700

    about to finish

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -44,9 +44,9 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-			s[cmpt] = a[i][j];
+			s[cmpt] = av[i][j];
 
-		s[cmpt] = '\n'
+		s[cmpt] = '\n';
 		cmpt++;
 	}
 	s[cmpt] = '\0';
