7e33125ac3dbc9c0c72a9e809cf2294ac8d3ced6
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-09-07 15:01:05 -0700

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * -puts - prints a string, follwed a new line,
+ * _puts - prints a string, follwed a new line,
  * @str: pointer to the string to print
  * Return: void
 */
@@ -9,6 +9,7 @@
 void _puts(char *str)
 {
 	int i = 0;
+
 	while (str[i])
 	{
 		_putchar(str[i]);
@@ -37,3 +38,52 @@ int _atoi(const char *s)
 	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints an integer
+ * @n: int
+ * Return: 0
+*/
+
+void print_int(unsigned long int n)
+{
+
+	unsigned long int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+
+}
+
+/**
+ * main - print the result of the multipliction, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
