24c99aa39d3c4f065f5fd4747d808677ecb54dc6
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-09-07 11:11:39 -0700

    dkdlf

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,13 +2,11 @@
 
 /**
  * *_memset - fills memory with a constant byte.
- *
  * @s: pointer to put the constant
  * @b: constant
  * @n: max bytes to use
- *
  * Return: s
- */
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -22,12 +20,10 @@ char *_memset(char *s, char b, unsigned int n)
 
 /**
  * *_calloc - allocates memory for an array, using malloc
- *
  * @nmemb: array length
  * @size: size of each element
- *
  * Return: pointer
- */
+*/
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
