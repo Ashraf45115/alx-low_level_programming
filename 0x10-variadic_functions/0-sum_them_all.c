de9495335618b3116e9555104f08333910d9e5ea
Ashraf45115 <ashrafahmed45115@gmail.com>
2023-09-14 09:47:07 -0700

    my first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_function.h"
+
+/**
+ * sum_them_all - sums variable arguments
+ * @n: the number of arguments
+ * @...: the integers to sum
+ * Return: the integer sum
+*/
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int a = 0, b = n;
+	va_list as;
+
+	if (!n)
+		return (0);
+	va_start(as, n);
+	while (b - 1)
+		a += va_arg(as, int);
+	va_end(as);
+	return (a);
+}
