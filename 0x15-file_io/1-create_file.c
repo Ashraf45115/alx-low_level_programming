0f4afb2188a0654499179208f97de5628ef3b3ae
Ashraf45115 <ashrafahmed45115@gmail.com>
2024-02-12 14:51:42 -0800

    fixed the return error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
-		return (0);
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CEART | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
