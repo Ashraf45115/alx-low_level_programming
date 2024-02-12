47bc5b5d61da25abc6dc06821234e4002411041c
Ashraf45115 <ashrafahmed45115@gmail.com>
2024-02-12 15:46:21 -0800

    hard work

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int _strlen(char *s)
  * create_file - a function that creates a file
  * @filename: name of the file to create
  * @text_content: text to write
- * Return: 1 on seccess -1 on failure
+ * Return: 1 on seccess 0 on failure
  */
 
 int create_file(const char *filename, char *text_content)
