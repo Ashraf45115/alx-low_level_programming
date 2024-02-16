50d8d585d9b54dfb8c85d12a3b514ad63146daf5
Ashraf45115 <ashrafahmed45115@gmail.com>
2024-02-16 03:21:34 -0800

    fixed a code after a while

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,14 +30,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CEART | O_TRUNC, S_IRUSR | S_IWUSR);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
 
 	close(fd);
-
 	return (bytes == len ? 1 : -1);
 }
