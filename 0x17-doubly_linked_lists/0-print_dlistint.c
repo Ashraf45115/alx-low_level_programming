63ffcaeb916fb6c3263025b60917911899f91f93
Ashraf45115 <ashrafahmed45115@gmail.com>
2024-03-21 16:54:12 -0700

    first commit doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the element of dlint list
+ * print_dlistint - prints doubly-linked list
  * @h: address of head node
- * Return: size f list
+ *
+ * Return: size of list
 */
 size_t print_dlistint(const dlistint_t *h)
 {
